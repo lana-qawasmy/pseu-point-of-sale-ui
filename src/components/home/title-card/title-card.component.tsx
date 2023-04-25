d09d80352c6c28334dae47a2956a9c101139b761
Khaldoun halawani <khalawani@sadasol.com>
2023-04-25 13:17:39 +0200

    POS-14 Created the card, with style_

---
+++ src/components/home/title-card/title-card.component.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./title-card.css";
+
+const TitleCard = () => {
+  return (
+    <div className="main-card">
+      <div className="title">
+        <h1>Point Of Saless App</h1>
+      </div>
+      <div className="logo">
+      </div>
+    </div>
+  )
+}
+
+export default TitleCard
