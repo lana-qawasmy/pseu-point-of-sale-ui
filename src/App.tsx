7c3c6399c47717d50037ad4289706ebe50c3060a
Khaldoun halawani <khalawani@sadasol.com>
2023-04-25 13:03:40 +0200

    POS-14 Created the card, with style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import TitleCard from './components/home/title-card/title-card.component'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <TitleCard />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
