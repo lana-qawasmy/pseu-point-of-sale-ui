ad28f58a24cf9df02c5c392a0741d7efb6042bcf
abdullah-home <181004@ppu.edu.ps>
2023-05-02 02:53:00 +0300

    finish the styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import './App.css';
-import Dropdown from './components/core/dropdown/dropdown.component';
+
 const App = () => {
     return (
         <div className="App">
-        <Dropdown />
         </div>
     );
 };
--- src/components/core/dropdown/dropdown.component.tsx
+++ src/components/core/dropdown/dropdown.component.tsx
@@ -1,22 +1,54 @@
 import './dropdown.css';
 interface IProps {
-    name?: string,
-    id?: string,
+    data: string[],
+    name: string,
+    id: string,
+    Placeholder?: string,
     Color?: string,
     Required?: boolean,
     Disabled?: boolean,
-    Radius?: string
+    Radius?: string,
+    width?: string,
+    height?: string,
+    onChange?: (val: string) => void
 }
 const Dropdown = (props: IProps) => {
-    // props = {...props, Color:"#0077B6", Disabled: false, Radius:"0",Required: false}
+    let style = {
+        border: `solid 2px ${props.Color || "#0077B6"}`,
+        color: `${props.Color || "#0077B6"}`,
+        width: `${props.width ? props.width + "px" : "150px"}`,
+        height: `${props.height ? props.height + "px" : "30px"}`,
+        borderRadius: `${props.Radius ? props.Radius + "px" : "0px"}`
+    };
+
     return (
-        <div className="dropdownWrapper">
-            <select id={props.id} name= {props.name}>
-                <option className='dropdownOption' value="volvo">Volvo</option>
-                <option className='dropdownOption' value="saab">Saab</option>
-                <option className='dropdownOption' value="opel">Opel</option>
-                <option className='dropdownOption' value="audi">Audi</option>
-            </select>
+        <div className='dropDown' >
+            <div className='dropdownContainer'
+                style={{ width: `${props.width ? props.width + "px" : "150px"}` }}>
+                {props.Required ?
+                    <div className="drpodpwnRequired"
+                        style={{ width: `${props.width ? props.width + "px" : "150px"}` }}>
+                        <span>*</span>
+                    </div>
+                    :
+                    <div className="drpodpwnRequired"
+                        style={{ width: `${props.width ? props.width + "px" : "150px"}` }}>
+                    </div>
+                }
+                <select id={props.id} name={props.name} className="dropdownWrapper"
+                    style={style} onChange={e => { return props.onChange && props.onChange(e.target.value) }}
+                    required={props.Required} disabled={props.Disabled}>
+                    {
+                        props.Placeholder &&
+                        <option className='dropdownOption' value="" disabled hidden>Role</option>
+                    }
+                    {props.data?.map((item) => {
+                        return (
+                            <option className='dropdownOption' value={item} key={item}>{item}</option>
+                        );
+                    })}
+                </select>
+            </div>
         </div>
     );
 };
