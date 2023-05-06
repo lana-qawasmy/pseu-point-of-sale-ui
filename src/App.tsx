48aebe65f4cd7f9cd86291efea48adede55b9c7b
AbdullahNuaimi <181004@ppu.edu.ps>
2023-05-06 18:03:36 +0300

    add disabled button style

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,24 @@ const App = () => {
             <div style={{margin: "40px"}}></div>
             <h1>Primary</h1>
             <Button HtmlType='button' Type='Primary' />
+            &nbsp;
+            <Button HtmlType='button' Type='Primary' Disabled/>
             <h1>Secondary</h1>
             <Button HtmlType='button' Type='Secondary' />
+            &nbsp;
+            <Button HtmlType='button' Type='Secondary' Disabled/>
             <h1>Tertiary</h1>
             <Button HtmlType='button' Type='Tertiary' />
+            &nbsp;
+            <Button HtmlType='button' Type='Tertiary' Disabled/>
             <h1>Danger</h1>
             <Button HtmlType='button' Type='Danger' />
+            &nbsp;
+            <Button HtmlType='button' Type='Danger' Disabled/>
             <h1>Ghost</h1>
             <Button HtmlType='button' Type='Ghost' />
+            &nbsp;
+            <Button HtmlType='button' Type='Ghost' Disabled/>
             <br />
             <br />
             <br />
