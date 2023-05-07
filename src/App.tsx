1bd2960e0f7f9dc78c2c0c0e703032f46b604ccf
abdullah-nuaimi <181004@ppu.edu.ps>
2023-05-07 12:04:18 +0300

    solve the conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Button from './components/core/button/button.component';
+import { Signin, ViewExistedItems, Signup, AddItem, PosView, OrdersHistory } from './pages/index';
 
 const App = () => {
     return (
         <div className="App">
-            <div style={{margin: "40px"}}></div>
-            <h1>Primary</h1>
-            <Button HtmlType='button' Type='Primary' Width='150' Ratio='50/10' Radius='15'>
-                Signup
-            </ Button>
-            &nbsp;
-            <Button HtmlType='button' Type='Primary' Disabled/>
-            <h1>Secondary</h1>
-            <Button HtmlType='button' Type='Secondary' />
-            &nbsp;
-            <Button HtmlType='button' Type='Secondary' Disabled/>
-            <h1>Tertiary</h1>
-            <Button HtmlType='button' Type='Tertiary' />
-            &nbsp;
-            <Button HtmlType='button' Type='Tertiary' Disabled/>
-            <h1>Danger</h1>
-            <Button HtmlType='button' Type='Danger' />
-            &nbsp;
-            <Button HtmlType='button' Type='Danger' Disabled/>
-            <h1>Ghost</h1>
-            <Button HtmlType='button' Type='Ghost' />
-            &nbsp;
-            <Button HtmlType='button' Type='Ghost' Disabled/>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<PosView />} />
+                    <Route path='/signin' element={<Signin />} />
+                    <Route path='/signup' element={<Signup />} />
+                    <Route path='/addItem' element={<AddItem />} />
+                    <Route path='/ordersHistory' element={<OrdersHistory />} />
+                    <Route path='/existedItems' element={<ViewExistedItems />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 };
