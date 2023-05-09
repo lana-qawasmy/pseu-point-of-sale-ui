71279bf7339551f4bd6d9cf21ca39e7c02aff251
Momen-G-Ar-Official <cp.momen@gmail.com>
2023-05-09 18:18:24 +0300

    add index.ts to the core folder

---
+++ src/components/core/index.ts
@@ -0,0 +1,3 @@
+export { default as Button } from './button/button.component';
+export { default as Dropdown } from './dropdown/dropdown.component';
+export { default as Input } from './input/input.component';
