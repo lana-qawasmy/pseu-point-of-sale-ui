d950e31db59f30d140f91273fe838eeffedfeaec
Momen-G-Ar-Official <cp.momen@gmail.com>
2023-05-09 22:30:23 +0300

    Add user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,23 @@
+import { UserNS } from '../types';
+
+const createUser = async (user: UserNS.User) => {
+    return fetch(`${process.env.REACT_APP_SERVER_URL}/user/signup`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+    }).then((response: Response) => {
+        if (response.status === 201)
+            return true;
+        else
+            return false;
+    }).catch((error: Error) => {
+        console.error(error.message);
+        return false;
+    });
+};
+
+export default {
+    createUser,
+};
