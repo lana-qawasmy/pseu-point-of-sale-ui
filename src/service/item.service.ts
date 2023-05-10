1fba518c3bfdb6b1e66c5757cba745c3dd7fd538
abdullah-home <181004@ppu.edu.ps>
2023-05-10 23:40:58 +0300

    asdasd

--- src/service/item.service.ts
+++ src/service/item.service.ts
@@ -2,14 +2,18 @@ import { ItemNS } from "../types";
 const addItem = async (item: ItemNS.Item) => {
   try {
     const stringResponse = await fetch(
-      `${process.env.REACT_APP_SERVER_URL}/item/addItem`,
+      `${process.env.REACT_APP_API_URL}/item/addItem`,
       {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
         method: "POST",
         body: JSON.stringify(item),
       }
     );
-    console.log("here's addItem response: ", await stringResponse.json());
-    return await stringResponse.json();
+    console.log("here's addItem response: ",  stringResponse);
+    return stringResponse;
   } catch (error) {
     console.log("failed to send the request! the error: ",error);
     return false;
