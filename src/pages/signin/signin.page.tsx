7b5f3b5a3a09d95abebfbc7c99a11a9a41083f8a
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-05-10 17:33:06 +0300

    set up signin page

--- src/pages/signin/signin.page.tsx
+++ src/pages/signin/signin.page.tsx
@@ -1,6 +1,35 @@
+import './signin.css'
+import mainPic from '../../assets/main-page pic.svg'
+import logo from '../../assets/partial-logo.png'
+import Input from '../../components/core/input/input.component';
+import PasswordInput from '../../components/core/password-input/password-input.component';
+import Button from '../../components/core/button/button.component';
+import { Link } from 'react-router-dom';
+
 const Signin = () => {
     return (
-        <div>Signin</div>
+        <div className='signin'>
+            <div className='signinWrapper'>
+                <img src={logo} alt="" />
+                <form className='signinForm'>
+                    <Input Height={40} Width={360} Radius={15} PlaceHolder='Email' />
+                    <PasswordInput height={40} Width={360} Radius={15} Placeholder='Password' />
+                    <Button
+                        HtmlType='submit'
+                        FontWeight={'inherit'}
+                        FontSize='16'
+                        Ratio='100/10'
+                        Width='380'
+                        Radius='20'>Login</Button>
+                </form>
+                <h3>
+                    Don't have an account?
+                    <Link to={'/signiup'}>Signup</Link>
+                </h3>
+            </div>
+            <img src={mainPic} alt="main pic" className='mainPic' />
+
+        </div>
     );
 };
 export default Signin;
