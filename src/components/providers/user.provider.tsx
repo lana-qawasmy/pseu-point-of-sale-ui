c776a91456cb9324df227197950fdcadb864e21d
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-06-01 20:40:17 +0300

    create userProvider

---
+++ src/components/providers/user.provider.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { UserNS } from "../../types";
+
+interface IProps {
+    children: React.ReactNode
+}
+interface IState {
+    setUser?: React.Dispatch<React.SetStateAction<UserNS.User | undefined>>,
+    user?: UserNS.User
+}
+
+export const UserContext: React.Context<IState> = React.createContext({});
+const UserProvider = (props: IProps) => {
+    const [user, setUser] = useState<UserNS.User>();
+
+    return (
+        <UserContext.Provider value={{ user: user, setUser: setUser }}>
+            {props.children}
+        </UserContext.Provider>
+    )
+
+}
+export default UserProvider;
--- src/hooks/signin.hook.ts
+++ src/hooks/signin.hook.ts
@@ -1,20 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import { login } from "../services";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { UserContext } from "../components/providers/user.provider";
 
 const useSignin = () => {
     const navigate = useNavigate();
+    const userContext = useContext(UserContext);
     const [validLogin, setValidLogin] = useState<boolean>(true);
     const handleValidation = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('submitted')
         const email = e.currentTarget.email.value;
         const password = e.currentTarget.password.value;
         const user = await login(email, password);
-        console.log(user);
+
+        if (userContext.setUser) {
+            userContext.setUser(user[0]);
+        }
+
         if (user?.length) {
             setValidLogin(true)
-            navigate('/' , {replace : true})
+            navigate('/', { replace: true })
             return true;
         } else {
             setValidLogin(false)
