6db64087378b8d2fcd03559c1025dc17c4b3049c
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-06-01 22:52:18 +0300

    export core components from index.ts

--- src/pages/signin/signin.page.tsx
+++ src/pages/signin/signin.page.tsx
@@ -1,9 +1,7 @@
 import './signin.css'
 import mainPic from '../../assets/main-page pic.svg'
 import logo from '../../assets/partial-logo.png'
-import Input from '../../components/core/input/input.component';
-import PasswordInput from '../../components/core/password-input/password-input.component';
-import Button from '../../components/core/button/button.component';
+import { Input, PasswordInput, Button } from '../../components/core'
 import { Link } from 'react-router-dom';
 import { useSignin } from '../../hooks';
 
@@ -15,7 +13,7 @@ const Signin = () => {
                 <img src={logo} alt="" />
                 <form className='signinForm' onSubmit={signin.handleValidation}>
                     <Input Height={40} Width={360} Radius={15} PlaceHolder='Email' name='email' />
-                    <PasswordInput height={40} Width={360} Radius={15} Placeholder='Password' name='password' state='none'/>
+                    <PasswordInput height={40} Width={360} Radius={15} Placeholder='Password' name='password' state='none' />
                     {!signin.validLogin && <span>Email or password is incorrect , please try again.</span>}
                     <Button
                         HtmlType='submit'
@@ -29,7 +27,7 @@ const Signin = () => {
                     </Button>
                 </form>
                 <p>
-                    Don't have an account? 
+                    Don't have an account?
                     <Link to={'/signup'}>Signup</Link>
                 </p>
             </div>
