f6e7da3007bb64906a79112d2b251aa5a54b6893
AbdullahNuaimi <181004@ppu.edu.ps>
2023-06-05 20:42:24 +0300

    move navbar under userContext

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ const App = () => {
     return (
         <div className="App">
             <BrowserRouter>
-                <NavigationBar />
                 <UserProvider>
+                <NavigationBar />
                     <Routes>
                             <Route path='/' element={<PosView />} />
                             <Route path='/signin' element={<Signin />} />
