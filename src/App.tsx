d905741cd4b1b8ac2ab89a92c27b43e5678e32c2
AbdullahNuaimi <181004@ppu.edu.ps>
2023-06-05 20:39:56 +0300

    last edt to solve conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import {
-  Signin,
-  ViewExistedItems,
-  Signup,
-  AddItem,
-  PosView,
-  OrdersHistory,
-  Terms,
-} from "./pages/index";
-import { UserProvider } from "./components/providers";
-import { NavigationBar } from "./components/core";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import { Signin, ViewExistedItems, Signup, AddItem, PosView, OrdersHistory, Terms } from './pages/index';
+import { UserProvider } from './components/providers';
+import { NavigationBar } from './components/core';
 const App = () => {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <NavigationBar />
-        <UserProvider>
-          <Routes>
-            <Route path="/" element={<PosView />} />
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/addItem" element={<AddItem />} />
-            <Route path="/ordersHistory" element={<OrdersHistory />} />
-            <Route path="/existedItems" element={<ViewExistedItems />} />
-            <Route path="/terms" element={<Terms />} />
-          </Routes>
-        </UserProvider>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <NavigationBar />
+                <UserProvider>
+                    <Routes>
+                            <Route path='/' element={<PosView />} />
+                            <Route path='/signin' element={<Signin />} />
+                            <Route path='/signup' element={<Signup />} />
+                            <Route path='/addItem' element={<AddItem />} />
+                            <Route path='/ordersHistory' element={<OrdersHistory />} />
+                            <Route path='/existedItems' element={<ViewExistedItems />} />
+                            <Route path='/terms' element={<Terms />} />
+                    </Routes>
+                </UserProvider>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
