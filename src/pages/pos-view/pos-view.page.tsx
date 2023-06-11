6bfd03cc9b3b7652744e4c8ebd78bbb1cbf19970
abdullah-nuaimi <181004@ppu.edu.ps>
2023-06-11 13:37:44 +0300

    finish layout

--- src/pages/pos-view/pos-view.page.tsx
+++ src/pages/pos-view/pos-view.page.tsx
@@ -1,14 +1,22 @@
 import './pos-view.css';
-
+import { Link } from 'react-router-dom';
 
 const PosView = () => {
     return (
         <div className='posViewContainer'>
-
-            <div className="posViewTitle">
-                Category
+            <div className="posTitlewrapper">
+                <div className="posViewTitle">
+                    Category
+                </div>
+                <div className="seeAllWrapper">
+                    <Link to={'/viewCategories'}>See All</Link>
+                </div>
             </div>
-
+            <div className="categoryBarContainer">
+                <div className="allBlock"></div>
+                <div className="categoryBar"></div>
+                <div className="addNewBlock"></div>
+            </div>
         </div>
     );
 };
