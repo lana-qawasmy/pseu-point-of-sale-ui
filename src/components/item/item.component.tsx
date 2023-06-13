c4412514123e2aa6dcefb8a64913d061ed3b8e0e
Momen-G-Ar <cp.momen@gmail.com>
2023-06-13 14:01:08 +0300

    Edit the ui for delete, edit, check

--- src/components/item/item.component.tsx
+++ src/components/item/item.component.tsx
@@ -3,6 +3,7 @@ import { ItemNS } from '../../types';
 import { GoTrashcan } from 'react-icons/go';
 import { GrCheckbox, GrCheckboxSelected } from 'react-icons/gr';
 import { FiEdit } from 'react-icons/fi';
+import React from 'react';
 interface IProps {
     item: ItemNS.Item;
     Selected?: boolean;
@@ -14,7 +15,7 @@ interface IProps {
 
 const Item = (props: IProps) => {
     const { _id, name, image, priceHistory, addedBy } = props.item;
-
+    const [displayACK, setDisplayACK] = React.useState(false);
     return (
         <div className={`mainItemContainer ${props.Selected ? 'isSelected' : ''}`}>
             <img src={image} alt="name" />
@@ -36,27 +37,106 @@ const Item = (props: IProps) => {
             </div>
             {
                 props.OnDelete && <span className='goTrashCan'>
-                    <GoTrashcan size={22} onClick={() => props.OnDelete && props.OnDelete(addedBy, _id as string)} color='#e0e2e8' />
+                    <GoTrashcan
+                        size={22}
+                        id={_id + '1'}
+                        onMouseOver={() => {
+                            let x = document.getElementById(_id + '1');
+                            if (x) {
+                                x.style.color = '#023e8a';
+                                x.style.stroke = '#023e8a';
+                            }
+                        }}
+                        onMouseLeave={() => {
+                            let x = document.getElementById(_id + '1');
+                            if (x) {
+                                x.style.color = '#e0e2e8';
+                                x.style.stroke = '#e0e2e8';
+                            }
+                        }}
+                        onClick={() => setDisplayACK(!displayACK)}
+                    />
                 </span>
+            }
+            {
+                displayACK &&
+                <div className='deleteACK'>
+                    <button className='ACKButton' onClick={() => props.OnDelete && props.OnDelete(addedBy, _id as string)}>
+                        YES
+                    </button>
+                    <button className='ACKButton' onClick={() => setDisplayACK(false)}>
+                        NO
+                    </button>
+                </div>
             }
             {
                 (!props.Selected) && props.OnSelect &&
                 <span className='grCheckbox'>
-                    <GrCheckbox size={20} onClick={() => props.OnSelect && props.OnSelect()} color='#e0e2e8' />
+                    <GrCheckbox
+                        size={20}
+                        id={_id + '2'}
+                        onMouseOver={() => {
+                            let x = document.getElementById(_id + '2');
+                            if (x && x.firstChild) {
+                                let y = x.firstChild as HTMLElement;
+                                y.style.stroke = '#023e8a';
+                            }
+                        }}
+                        onMouseLeave={() => {
+                            let x = document.getElementById(_id + '2');
+                            if (x && x.firstChild) {
+                                let y = x.firstChild as HTMLElement;
+                                y.style.stroke = '#e0e2e8';
+                            }
+                        }}
+                        onClick={() => props.OnSelect && props.OnSelect()} color='#e0e2e8'
+                    />
                 </span>
             }
             {
                 props.Selected && props.OnSelect &&
                 <span className='grCheckbox'>
-                    <GrCheckboxSelected size={20} onClick={() => props.OnSelect && props.OnSelect()} color='#e0e2e8' />
+                    <GrCheckboxSelected
+                        size={20}
+                        color='#023e8a'
+                        onClick={() => props.OnSelect && props.OnSelect()}
+                    />
                 </span>
             }
             {
                 props.OnEdit &&
                 <span className='fiEdit'>
-                    <FiEdit size={20} onClick={() => props.OnEdit && props.OnEdit()} color='#e0e2e8' />
+                    <FiEdit
+                        size={20}
+                        id={_id + '3'}
+                        onMouseOver={() => {
+                            let x = document.getElementById(_id + '3');
+                            if (x && x.firstChild) {
+                                let y = x as HTMLElement;
+                                if (y.getElementsByTagName('path').length > 0) {
+                                    let yy = y.getElementsByTagName('path')[0].style;
+                                    yy.stroke = '#023e8a';
+                                    yy = y.getElementsByTagName('path')[1].style;
+                                    yy.stroke = '#023e8a';
+                                }
+                            }
+                        }}
+                        onMouseLeave={() => {
+                            let x = document.getElementById(_id + '3');
+                            if (x && x.firstChild) {
+                                let y = x as HTMLElement;
+                                if (y.getElementsByTagName('path').length > 0) {
+                                    let yy = y.getElementsByTagName('path')[0].style;
+                                    yy.stroke = '#e0e2e8';
+                                    yy = y.getElementsByTagName('path')[1].style;
+                                    yy.stroke = '#e0e2e8';
+                                }
+                            }
+                        }}
+                        onClick={() => props.OnEdit && props.OnEdit()} color='#e0e2e8' />
                 </span>
             }
+
         </div>
     );
 };
