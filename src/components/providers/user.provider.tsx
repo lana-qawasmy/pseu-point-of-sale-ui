a65d5c936a3085ea17db7a898c4fc45d5f3886a9
Momen-G-Ar <cp.momen@gmail.com>
2023-06-13 18:41:28 +0300

    Add user to the session storage

--- src/components/providers/user.provider.tsx
+++ src/components/providers/user.provider.tsx
@@ -13,7 +13,12 @@ interface IState {
 export const UserContext: React.Context<IState> = React.createContext({});
 const UserProvider = (props: IProps) => {
     const [user, setUser] = useState<UserNS.User>();
-
+    React.useMemo(() => {
+        let u = sessionStorage.getItem('user');
+        if (u) {
+            setUser(JSON.parse(u));
+        }
+    }, []);
     return (
         <UserContext.Provider value={{ user: user, setUser: setUser }}>
             {props.children}
