24a50a8c67a2e148d63f3fd606b86878cb508f44
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-06-14 10:31:13 +0300

    search bar activation

---
+++ src/hooks/params.hook.ts
@@ -0,0 +1,30 @@
+import { useContext, useEffect } from "react";
+import { useSearchParams } from "react-router-dom"
+import { itemService } from "../services";
+import { UserContext } from "../components/providers/user.provider";
+
+const useParams = () => {
+    const [params, setParams] = useSearchParams();
+    const userContext = useContext(UserContext)
+    const setParamsOverride = (name: string, value: string) => {
+        const newParams = new URLSearchParams(params);
+        newParams.set(name, value);
+        setParams(newParams);
+    }
+    useEffect(() => {
+        itemService.getItems(
+            userContext.user?._id as string,
+            userContext.user?.token as string,
+            params.get('searchTerms') || '')
+        // eslint-disable-next-line 
+    }, [params])
+
+    return {
+        params,
+        setParams: setParamsOverride
+    }
+
+
+}
+
+export default useParams
