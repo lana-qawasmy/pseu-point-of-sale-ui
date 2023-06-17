c7b34b7171e8d1693e03df32afedad8f032cdaa1
AbdullahNuaimi <181004@ppu.edu.ps>
2023-06-17 20:55:26 +0300

    fixe layout

--- src/pages/view-existed-items/view-existed-items.page.tsx
+++ src/pages/view-existed-items/view-existed-items.page.tsx
@@ -1,62 +1,70 @@
-import './view-existed-items.css';
-import { Item } from '../../components';
-import { CategoryBar } from '../../components';
-import { useEffect, useState } from 'react';
-import { useParam, useViewItems } from '../../hooks';
-import React from 'react';
-import { SearchBar } from '../../components/core';
+import "./view-existed-items.css";
+import { Item } from "../../components";
+import { CategoryBar } from "../../components";
+import { useEffect, useState } from "react";
+import { useParam, useViewItems } from "../../hooks";
+import React from "react";
+import { SearchBar } from "../../components/core";
 
 const ViewExistedItems = () => {
-    const { itemsTable, select, handleChangeSelect, handleDelete, handleChangeCategory } = useViewItems();
-    const [categoryId, setCategoryId] = useState<string>('');
-    const [selectedCategoryItems, setSelectedCategoryItems] = useState<[string]>(['']);
-    useEffect(() => {
-        handleChangeCategory(selectedCategoryItems);
-        // eslint-disable-next-line
-    }, [selectedCategoryItems, categoryId]);
+  const {
+    itemsTable,
+    select,
+    handleChangeSelect,
+    handleDelete,
+    handleChangeCategory,
+  } = useViewItems();
+  const [categoryId, setCategoryId] = useState<string>("");
+  const [selectedCategoryItems, setSelectedCategoryItems] = useState<[string]>([
+    "",
+  ]);
+  useEffect(() => {
+    handleChangeCategory(selectedCategoryItems);
+    // eslint-disable-next-line
+  }, [selectedCategoryItems, categoryId]);
 
+  const useParams = useParam();
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    useParams.setParams("searchTerms", e.target.value);
+  };
 
-    const useParams = useParam();
-    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-        useParams.setParams('searchTerms', e.target.value);
-    };
-    
-    return (
-        <div className='viewItemsPage'>
-            <CategoryBar
-                disableAddBlock={false}
-                categoryId={categoryId}
-                setCategoryId={setCategoryId}
-                selectedCategoryItems={selectedCategoryItems}
-                setSelectedCategoryItems={setSelectedCategoryItems} 
-                />
-            <h2>Menu</h2>
-            <SearchBar
-                Name='search'
-                Id='search'
-                OnChange={handleSearch}
-                Value={useParams.params.get('searchTerms') || ''}
-                Padding={12}
-                Raduis={5}
+  return (
+    <div className="viewItemsPage">
+      <CategoryBar
+        disableAddBlock={false}
+        categoryId={categoryId}
+        setCategoryId={setCategoryId}
+        selectedCategoryItems={selectedCategoryItems}
+        setSelectedCategoryItems={setSelectedCategoryItems}
+      />
+      <div className="searchBarWrapper">
+        <SearchBar
+          Name="search"
+          Id="search"
+          OnChange={handleSearch}
+          Value={useParams.params.get("searchTerms") || ""}
+          Raduis={5}
+          Height={35}
+          Width={280}
+          FontSize={20}
+        />
+      </div>
+      <div className="itemsContainer">
+        {itemsTable.map((item, index) => {
+          return (
+            <Item
+              key={item._id}
+              Selected={select[index]}
+              item={item}
+              DeletedPrice={12}
+              OnDelete={handleDelete}
+              OnEdit={() => console.log(1)}
+              OnSelect={() => handleChangeSelect(index, categoryId)}
             />
-            <div className='itemsContainer'>
-                {
-                    itemsTable.map((item, index) => {
-                        return (
-                            <Item
-                                key={item._id}
-                                Selected={select[index]}
-                                item={item}
-                                DeletedPrice={12}
-                                OnDelete={handleDelete}
-                                OnEdit={() => console.log(1)}
-                                OnSelect={() => handleChangeSelect(index, categoryId)}
-                            />
-                        );
-                    })
-                }
-            </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default ViewExistedItems;
