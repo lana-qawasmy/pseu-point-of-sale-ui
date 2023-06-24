2cfea1620d0deb14c70b596fa7ed1f0f6029e990
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-06-24 21:11:46 +0300

    resolving conflict in /components/index.ts

---
+++ src/components/item-in-pos/item-in-pos.component.tsx
@@ -0,0 +1,70 @@
+import './item-in-pos.css';
+import { CollectionNS, ItemNS } from '../../types';
+import { GoLinkExternal } from 'react-icons/go';
+import { NavigateFunction } from 'react-router-dom';
+
+interface IProps {
+    selectedCategory: CollectionNS.ICollection | null;
+    item: ItemNS.Item;
+    DeletedPrice?: number;
+    OnSelect: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+    navigate: NavigateFunction;
+}
+
+const ItemInPOS = (props: IProps) => {
+    const { _id, name, image, priceHistory } = props.item;
+
+    const handleLinkClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        e.stopPropagation();
+        props.navigate(`/viewSingleItem/${_id}`, { replace: false });
+    };
+
+    return (
+        <div
+            id={_id + 'elementInPOS'}
+            className='mainItemInPOSContainer'
+            onClick={(e) => props.OnSelect(e)}
+        >
+            <div className='goExternalLink' onClick={(e) => handleLinkClick(e)}>
+                <GoLinkExternal
+                    id={'goExternalLink' + _id}
+                    size={20}
+                    color='#e0e2e8'
+                    onMouseOver={() => {
+                        let x = document.getElementById('goExternalLink' + _id);
+                        if (x && x.firstChild) {
+                            let y = x.firstChild as HTMLElement;
+                            y.style.color = '#023e8a';
+                        }
+                    }}
+                    onMouseLeave={() => {
+                        let x = document.getElementById('goExternalLink' + _id);
+                        if (x && x.firstChild) {
+                            let y = x.firstChild as HTMLElement;
+                            y.style.color = '#e0e2e8';
+                        }
+                    }}
+                />
+            </div>
+            <div style={{ backgroundImage: `url('${image}')` }} className='itemImage' />
+            <div className='infoWrapper'>
+                <span className='itemName'>
+                    {name}
+                </span>
+                <div className='itemPrice'>
+                    <span className='currentItemPrice'>
+                        {priceHistory[0].price && priceHistory[0].price.toFixed(2)} &nbsp;&nbsp;
+                    </span>
+                    {
+                        props.DeletedPrice &&
+                        <span className='deletedItemPrice'>
+                            {props.DeletedPrice.toFixed(2)}
+                        </span>
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ItemInPOS;
