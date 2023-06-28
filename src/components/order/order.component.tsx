26e39460355c1a88a38c313225d0862d518c0379
Momen-G-Ar <cp.momen@gmail.com>
2023-06-28 20:27:33 +0300

    Edit style of the order component

--- src/components/order/order.component.tsx
+++ src/components/order/order.component.tsx
@@ -10,13 +10,15 @@ interface IProps {
 
 const Order = (props: IProps) => {
     const { orderNo, cashierName, total, time, date } = props;
+    const timeArray = time.split(':');
+    let splitTime = timeArray[0] + ':' + timeArray[1] + ' ' + timeArray[2][3] + timeArray[2][4];
     return (
         <tr className='orderContainer'>
             <td>{orderNo}</td>
             <td>{cashierName}</td>
             <td>{total.toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 1 })}$</td>
-            <td>{time}</td>
-            <td>{date}</td>
+            <td className='silver'>{splitTime}</td>
+            <td className='silver'>{date}</td>
         </tr>
     );
 };
