3435376d29936b49143ffda3af0396b5b40c0a76
AbdullahNuaimi <181004@ppu.edu.ps>
2023-06-28 23:06:39 +0300

    finish styling & logic

--- src/components/edit-roles/edit-roles.component.tsx
+++ src/components/edit-roles/edit-roles.component.tsx
@@ -1,64 +1,60 @@
 import "./edit-roles.css";
-import { BsSearch } from 'react-icons/bs';
+import { BsSearch } from "react-icons/bs";
 import { useEditRoles } from "../../hooks";
+import { MdOutlineFileDownloadDone } from "react-icons/md";
 const EditRoles = () => {
-    const {
-        search, setSearch,
-        usersList,setUsersList,
-        handleChangeRole,
-    } = useEditRoles();
+  const {
+    search,
+    setSearch,
+    usersList,
+    setUsersList,
+    handleChangeRole,
+  } = useEditRoles();
   return (
     <div className="editRolesContainer">
+        <h1 style={{textAlign: "left"}}>Users List</h1>
       <div className="userSearchWrapper">
-        <input 
-        type="text" 
-        id="usersSearch" 
-        placeholder="Search"
-        value={search}
-        onChange={e=>{setSearch(e.target.value)}}
+        <input
+          type="text"
+          id="usersSearch"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
         />
-        <i className="fa fa-user icon"><BsSearch /></i>
+        <i className="fa fa-user icon">
+          <BsSearch />
+        </i>
       </div>
       <div className="usersListContainer">
-        <ul>
-            {
-                usersList?.map((user,index) =>{
-                    return(
-                        <li key={index}>
-                            <span key={index}>{user.fullName}</span>
-                            <select onChange={e => handleChangeRole(e.target.value, user, index)}>
-                                {user.role === 'cashier'&&
-                                <>
-                                <option value="cashier">Cashier</option>
-                                <option value="manager">Manager</option>
-                                </>
-                                }
-                                {user.role === 'manager'&&
-                                <>
-                                <option value="manager">Manager</option>
-                                <option value="cashier">Cashier</option>
-                                </>
-                                }
-                            </select>
-                        </li>
-                    )
-                })
-            }
-        </ul>
-        {/* <select onChange={e => handleChangeRole(e.target.value)}>
-            {
-                usersList?.map((user, index)=>{
-                    return (
-                        <option 
-                        value={user._id} 
-                        key={index} 
-                        >
-                            {user.fullName}
-                        </option>
-                        )
-                })
-            }
-        </select> */}
+          {usersList?.map((user, index) => {
+            return (
+              <div key={index}>
+                <span key={index}>{user.fullName}</span>
+                <div className="selectWrapper">
+                <select
+                  onChange={(e) =>
+                    handleChangeRole(e.target.value, user, index)
+                  }
+                >
+                  {user.role === "cashier" && (
+                    <>
+                      <option value="cashier">Cashier</option>
+                      <option value="manager">Manager</option>
+                    </>
+                  )}
+                  {user.role === "manager" && (
+                    <>
+                      <option value="manager">Manager</option>
+                      <option value="cashier">Cashier</option>
+                    </>
+                  )}
+                </select>
+                </div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
--- src/hooks/edit-roles.hook.ts
+++ src/hooks/edit-roles.hook.ts
@@ -3,12 +3,13 @@ import { useContext } from "react";
 import { UserContext } from "../components/providers/user.provider";
 import { UserNS } from "../types";
 import { updateUser } from "../services";
-
+import useNotification from "./notification.hook";
 import { rolesServices } from "../services";
 const useEditRoles = () =>{
     const {user} = useContext(UserContext);
     const [usersList, setUsersList] = useState<UserNS.User[]>([]);
     const [search,setSearch] = useState<string>('');
+    const {setNotification} = useNotification()
     const getUsers = async()=>{
         const users = await rolesServices.getUsers(user?.token || '', search);
         setUsersList([...users]);
@@ -16,7 +17,11 @@ const useEditRoles = () =>{
     };
     const updateUserRole = async(user: UserNS.User) => {
         const updatedUser = await updateUser(user);
-        console.log({updatedUser});
+        if(updatedUser){
+            setNotification({message: 'Role updated', status: 'success', autoClose: 2000});
+        }else{
+            setNotification({message: 'Something went wrong please try again', status: 'error', autoClose: 3000});
+        }
     }
     const handleChangeRole = (value: string, user: UserNS.User, index: number) =>{
         console.log('whaaat');
@@ -37,7 +42,7 @@ const useEditRoles = () =>{
     return{
         search,setSearch,
         usersList,setUsersList,
-        handleChangeRole
+        handleChangeRole,
     }
 }
 
