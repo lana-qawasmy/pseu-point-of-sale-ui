0dcce78dc534e3f59663507b887c2668fa957876
AbdullahNuaimi <181004@ppu.edu.ps>
2023-06-29 14:09:23 +0300

    style and logic for item description

--- src/hooks/singleItem.hook.ts
+++ src/hooks/singleItem.hook.ts
@@ -1,34 +1,45 @@
-import { useParams, useNavigate } from 'react-router-dom';
-import { itemService } from '../services';
-import { useContext } from 'react';
-import { UserContext } from '../components/providers/user.provider';
-import useNotification from './notification.hook';
-import React from 'react';
-const useSingleItem = () =>{
-    const navigate = useNavigate();
-    const params = useParams();
-    const id = params.id;
-    const {user} = useContext(UserContext);
-    const {setNotification} = useNotification();
-    const getItem = async () =>{
-        const item = await itemService.getItem(user?.token || '',id || '');
-        if(typeof(item) === 'object'){
-            console.log(item);
-        }else{
-            setNotification({message: item, status: 'error', autoClose: 2000});
-            navigate('/');
-        }
+import { useParams, useNavigate } from "react-router-dom";
+import { itemService } from "../services";
+import { useContext } from "react";
+import { UserContext } from "../components/providers/user.provider";
+import useNotification from "./notification.hook";
+import React from "react";
+import { ItemNS } from "../types";
+const useSingleItem = () => {
+  const navigate = useNavigate();
+  const params = useParams();
+  const id = params.id;
+  const { user } = useContext(UserContext);
+  const { setNotification } = useNotification();
+  const [item, setItem] = React.useState<ItemNS.Item>();
+  const getItem = async () => {
+    const response = await itemService.getItem(user?.token || "", id || "");
+    if (typeof response === "object") {
+      setItem({
+        addedBy: response.addedBy,
+        barcode: response.barcode,
+        description: response.description,
+        image: response.image,
+        name: response.name,
+        priceHistory: response.priceHistory,
+        _id: response._id,
+      });
+      console.log(item);
+    } else {
+      setNotification({ message: response, status: "error", autoClose: 2000 });
+      navigate("/");
     }
-    React.useMemo(()=>{
-        getItem();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
-    console.log(id);
+  };
+  React.useMemo(() => {
+    getItem();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  console.log(id);
 
-    return{
-        id,
-    }
-
-}
+  return {
+    id,
+    item,
+  };
+};
 
 export default useSingleItem;
--- src/pages/single-item/single-item.page.tsx
+++ src/pages/single-item/single-item.page.tsx
@@ -1,146 +1,34 @@
-import './single-item.css';
-import { useSingleItem } from '../../hooks';
-const SingleItem = () =>{
-    const {id} = useSingleItem();
-    return(
-        <>
-            <div className="singleItemPage">
-                <span>id: {id}</span>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
+import "./single-item.css";
+import { useSingleItem } from "../../hooks";
+const SingleItem = () => {
+  const { id, item } = useSingleItem();
+  return (
+    <>
+      {item && (
+        <div className="singleItemPage">
+          <div className="singleItemContainer">
+            <div
+              className="image"
+              style={{ backgroundImage: `url('${item.image}')` }}
+            ></div>
+            <div className="content">
+              <h2>{item.name}</h2>
+              <span className="quantity">
+                <span className="number">43</span>Pieces available
+              </span>
+              <div className="price">
+                <span className="certainPrice">
+                  {item.priceHistory[0].price.toFixed(2).toString()}
+                </span>
+                <span className="discount">22.05</span>
+              </div>
+              <span className="description">{item.description}</span>
             </div>
-        </>
-    );
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default SingleItem;
