9ae837913d65a30d7d7babbe1606a5573c316d0f
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-07-03 21:30:45 +0300

    prevent ordering sold out items

--- src/components/item-in-pos/item-in-pos.component.tsx
+++ src/components/item-in-pos/item-in-pos.component.tsx
@@ -2,6 +2,7 @@ import './item-in-pos.css';
 import { CollectionNS, ItemNS } from '../../types';
 import { GoLinkExternal } from 'react-icons/go';
 import { NavigateFunction } from 'react-router-dom';
+import { useNotification } from '../../hooks';
 
 interface IProps {
     selectedCategory: CollectionNS.ICollection | null;
@@ -13,7 +14,7 @@ interface IProps {
 
 const ItemInPOS = (props: IProps) => {
     const { _id, name, image, priceHistory, quantity } = props.item;
-
+    const notification = useNotification();
     const handleLinkClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         e.stopPropagation();
         props.navigate(`/viewSingleItem/${_id}`, { replace: false });
@@ -22,8 +23,12 @@ const ItemInPOS = (props: IProps) => {
     return (
         <div
             id={_id + 'elementInPOS'}
-            className='mainItemInPOSContainer'
-            onClick={(e) => props.OnSelect(e)}
+            className={quantity ? 'mainItemInPOSContainer' : 'mainItemInPOSContainer soldOut'}
+            onClick={(e) => {
+                quantity
+                    ? props.OnSelect(e)
+                    : notification.setNotification({message:'This item is out of stock!' , status:'error'})
+            }}
         >
             <div className='goExternalLink' onClick={(e) => handleLinkClick(e)}>
                 <GoLinkExternal
