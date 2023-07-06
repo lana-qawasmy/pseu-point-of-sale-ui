a9addf14c86a5028c67ea28a9a1ae322e6c2215c
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-07-07 00:20:20 +0300

    order item card set up

--- src/components/order-card/order-card.component.tsx
+++ src/components/order-card/order-card.component.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './order-card.css';
 import { ItemNS } from '../../types';
 
@@ -8,7 +7,14 @@ interface IProps {
 const OrderCard = (props: IProps) => {
     return (
         <div>
-            OrderItem
+            <img src={props.item?.image} alt={props.item?.name} />
+            <div className='itemInfo'>
+                <span>{props.item?.name}</span>
+                <div className="priceQuantity">
+                    <span># {props.item?.quantity}</span>
+                    <span> {props.item?.priceHistory[0].price.toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 1 })} $</span>
+                </div>
+            </div>
         </div>
     );
 };
