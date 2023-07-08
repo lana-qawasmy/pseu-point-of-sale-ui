3c5a3a676c92a03c29916dc1e9e0a517dc122f24
Momen-G-Ar <cp.momen@gmail.com>
2023-07-08 19:19:20 +0300

    Add titles to profile page

--- src/hooks/add-discount.hook.ts
+++ src/hooks/add-discount.hook.ts
@@ -1,62 +1,62 @@
 import React from "react";
-import { useState } from "react"
+import { useState } from "react";
 import { DiscountNS } from "../types";
 import { discountService } from "../services";
 import { useContext } from "react";
 import { UserContext } from "../components/providers/user.provider";
 import useNotification from "./notification.hook";
-export interface IInput{
+export interface IInput {
     code: string,
     percentage: number,
     interval: number,
 }
 
-const useAddDiscount = () =>{
-    const {user} = useContext(UserContext);
-    const {setNotification} = useNotification();
-    const [input,setInput] = useState<DiscountNS.Discount>({code: '', value: 0,daysInterval: 0});
+const useAddDiscount = () => {
+    const { user } = useContext(UserContext);
+    const { setNotification } = useNotification();
+    const [input, setInput] = useState<DiscountNS.Discount>({ code: '', value: 0, daysInterval: 0 });
     const [disableButton, setDisableButton] = useState<boolean>(true);
-    const handleNumberInput = (value: string,inputName: string)=>{
-        const num =parseInt(value)
-        if(num >= 0){
-            inputName === 'percentage'? 
-            setInput((oldState) => ({...oldState,value: num})) 
-            :
-            setInput((oldState) => ({...oldState,daysInterval: num})) 
+    const handleNumberInput = (value: string, inputName: string) => {
+        const num = parseInt(value);
+        if (num >= 0) {
+            inputName === 'percentage' ?
+                setInput((oldState) => ({ ...oldState, value: num }))
+                :
+                setInput((oldState) => ({ ...oldState, daysInterval: num }));
         }
-    }
-    React.useMemo(()=>{
-        if(input.code !== '' && input.daysInterval !== 0 && input.value !==0){
+    };
+    React.useMemo(() => {
+        if (input.code !== '' && input.daysInterval !== 0 && input.value !== 0) {
             setDisableButton(false);
-        }else
-        setDisableButton(true);
-    },[input]);
+        } else
+            setDisableButton(true);
+    }, [input]);
 
-   const  handlSave = async() =>{
-    if(input.code !== '' && input.daysInterval !== 0 && input.value !==0){
-        try {
-            const response = await discountService.addDiscount(input,user?.token || '');
-            if(response.success === true){
-                setNotification({message: response.message, status: 'success', autoClose: 2000});
-            }else{
-                setNotification({message: response.message, status: 'error', autoClose: 2000});
+    const handelSave = async () => {
+        if (input.code !== '' && input.daysInterval !== 0 && input.value !== 0) {
+            try {
+                const response = await discountService.addDiscount(input, user?.token || '');
+                if (response.success === true) {
+                    setNotification({ message: response.message, status: 'success', autoClose: 2000 });
+                } else {
+                    setNotification({ message: response.message, status: 'error', autoClose: 2000 });
+                }
+                emptyInputs();
+            } catch (error) {
+                console.error(error);
+                setNotification({ message: 'Server response timeout', status: 'error', autoClose: 2000 });
             }
-            emptyInputs();
-        } catch (error) {
-            console.error(error);
-            setNotification({message: 'Server response timeout', status: 'error', autoClose: 2000});
         }
-    }
-   }
-   const emptyInputs = () =>{
-    setInput({value: 0,code: '',daysInterval: 0});
-   }
-    return{
-        input,setInput,
-        handleNumberInput,disableButton,
-        handlSave,emptyInputs
-    }
-}
+    };
+    const emptyInputs = () => {
+        setInput({ value: 0, code: '', daysInterval: 0 });
+    };
+    return {
+        input, setInput,
+        handleNumberInput, disableButton,
+        handelSave, emptyInputs
+    };
+};
 
 
-export default useAddDiscount
+export default useAddDiscount;
