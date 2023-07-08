15b6ecd5a13b08f0589c8b1111e917b77c0081f7
Momen-G-Ar <cp.momen@gmail.com>
2023-07-08 19:00:49 +0300

    Add titles to the single order page

--- src/pages/single-order/single-order.page.tsx
+++ src/pages/single-order/single-order.page.tsx
@@ -5,40 +5,40 @@ import { formatPrice } from '../../hooks';
 
 
 const SingleOrder = () => {
-    const { order, orderItems, splitDate, splitTime, subtotal , itemsNo } = useSingleOrder();
+    const { order, orderItems, splitDate, splitTime, subtotal, itemsNo } = useSingleOrder();
     return (
         <div className='singleOrderPage'>
             <div className="orderInfo">
                 <h1>Order #{order?.orderNumber}</h1>
                 <div className="wrapper">
                     <div className="left">
-                        <span>Cashier : </span>
-                        <span>Time : </span>
-                        <span>Date : </span>
-                        <span>Subtotal : </span>
-                        <span>Discount code : </span>
-                        <span>Tax(10%) : </span>
-                        <span style={{ fontSize: '24px', fontWeight: "bolder" }}>Total : </span>
+                        <span title='Cashier'>Cashier : </span>
+                        <span title='Time'>Time : </span>
+                        <span title='Date'>Date : </span>
+                        <span title='Subtotal'>Subtotal : </span>
+                        <span title='Discount code'>Discount code : </span>
+                        <span title='Tax'>Tax(10%) : </span>
+                        <span title='Total' style={{ fontSize: '24px', fontWeight: "bolder" }}>Total : </span>
                     </div>
                     <div className="right">
-                        <span className="name info">{order?.cashierName}</span>
-                        <span className="info grey">{splitTime}</span>
-                        <span className="info grey">{splitDate}</span>
-                        <span className="info">{formatPrice(subtotal)} $</span>
-                        <span className="info" style={{color: 'rgb(239, 71, 111'}} >{order?.discountCode || 'No discount!'}</span>
-                        <span className="info">{formatPrice(subtotal * 0.1)} $</span>
-                        <span className="info" >{formatPrice(order?.total as number || 0)} $</span>
+                        <span title={'Cashier name'} className="name info">{order?.cashierName}</span>
+                        <span title='Time' className="info grey">{splitTime}</span>
+                        <span title='Date' className="info grey">{splitDate}</span>
+                        <span title='Subtotal' className="info">{formatPrice(subtotal)} $</span>
+                        <span title='Discount code' className="info" style={{ color: 'rgb(239, 71, 111' }} >{order?.discountCode || 'No discount!'}</span>
+                        <span title='Tax' className="info">{formatPrice(subtotal * 0.1)} $</span>
+                        <span title='Total' className="info" >{formatPrice(order?.total as number || 0)} $</span>
                     </div>
                 </div>
             </div>
             <div className="orderDetails">
-                <h2>Order Details</h2>
+                <h2 title='Order details'>Order Details</h2>
                 <div className='OrderItems'> {
                     orderItems && orderItems.map(item =>
                         <OrderCard item={item} key={item._id} />
                     )}
                 </div>
-                <div className="itemsQuantity"># of items : {itemsNo} </div>
+                <div title='Number of items' className="itemsQuantity"># of items : {itemsNo} </div>
             </div>
         </div>
     );
