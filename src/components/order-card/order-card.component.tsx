ed38b8e8146611a8127eb82c1757e32f3b799f2b
raghad-ns <raghadnadersalem20022017@gmail.com>
2023-07-08 12:19:27 +0300

    price edits

--- src/components/order-card/order-card.component.tsx
+++ src/components/order-card/order-card.component.tsx
@@ -10,8 +10,8 @@ const OrderCard = (props: IProps) => {
             <img src={props.item?.image} alt={props.item?.name} />
             <div className='itemInfo'>
                 <span style={{ fontWeight: 'bolder', fontSize: '20px', textTransform: 'capitalize' }}>{props.item?.name}</span>
-                <span># {props.item?.quantity}</span>
-                <span> {props.item?.priceHistory[0].price.toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 1 })} $</span>
+                <span>#{props.item?.quantity}</span>
+                <span>${props.item?.priceHistory[0].price.toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 1 })}</span>
             </div>
         </div>
     );
